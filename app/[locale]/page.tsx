8e008e2fc5a9de31a750d820c0ee14e47f0926bb
HabibRazek <habibrazeg32@gmail.com>
2024-08-19 09:08:07 +0100

    unstable_setRequestLocale

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,9 +2,17 @@ import EstimateSection from "@/components/Landing/EstimateSection";
 import HeroSection from "@/components/Landing/HeroSection";
 import Footer from "@/components/Nav/Footer";
 import Navbar from "@/components/Nav/Navbar";
+import {unstable_setRequestLocale} from 'next-intl/server';
 
 
-export default function Home() {
+type Props = {
+  params: {locale: string};
+};
+
+
+export default function Home({params: {locale}}: Props) {
+  unstable_setRequestLocale(locale);
+
 
   return (
     <div>
